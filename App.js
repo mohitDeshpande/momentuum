5dbc2da92d58b2afc88180c5afe5bca6868c424b
kristkirp <kristkirp@mail.ru>
2018-02-20 17:01:16 -0500

    change case list path

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import Login from './screens/login/components/Login';
-import CaseList from './screens/caselist/CaseList';
+import CaseList from './screens/CaseList/components/CaseList';
 
 export default class App extends React.Component {
   render() {
