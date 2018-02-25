97369b493697c77cd58a9127b3833a8295753d56
Soaib Momin <smomin20@gmail.com>
2018-02-24 20:30:18 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,34 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import CaseDetails from "./components/CaseDetails/CaseDetails";
+import CaseDetails from "./screens/CaseDetails/components/CaseDetails/CaseDetails";
+import CaseUpdateForm from "./screens/CaseDetails/components/CaseDetails/CaseUpdateForm";
 import { Font } from 'expo';
+import { StackNavigator } from 'react-navigation';
+
+
+const defaultNavigationOptions = {
+  headerStyle: {
+    backgroundColor: '#2b6aa6',
+  },
+  headerTitleStyle:{ color: '#ffffff',textAlign: 'center',alignSelf:'center'},
+};
 
 export default class App extends React.Component {
 
-  state = {
-    fontLoaded: false,
-  };
-  async componentDidMount() {
-    await Font.loadAsync({
-      'Lato-Bold': require('./assets/fonts/Lato-Bold.ttf'),
-      'Lato-Regular': require('./assets/fonts/Lato-Regular.ttf'),
-    });
-
-    this.setState({ fontLoaded: true });
-  }
-
+  
   render() {
     return (
       <View style={styles.container}>
-      {
-        this.state.fontLoaded ? (
-          <CaseDetails/>
-        ) : null
-      }
+        <AppNavigation />
       </View>
     );
   }
 }
+
+const AppNavigation = StackNavigator(
+  {
+    CaseDetails: { screen: CaseDetails },
+    CaseUpdateForm: { screen: CaseUpdateForm },
+  },
+  {
+    headerMode: 'screen',
+    navigationOptions: {
+      ...defaultNavigationOptions,        
+      }
+    },
+);
 
 const styles = StyleSheet.create({
   container: {
