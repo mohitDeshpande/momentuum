00f0969655b211fbfed3eef9fe6fe22afd9ab8e6
Soaib Momin <smomin20@gmail.com>
2018-03-19 02:16:39 -0400

    update functionality solution

--- assets/config/endpoint.js
+++ assets/config/endpoint.js
@@ -1,6 +1,6 @@
 export default {
   api: {
-    url: "http://127.0.0.1:5000/api/",
+    url: "http://192.168.0.13/MomentuumApi/api/",
     endpoints: {
       login: "auth/login",
       caselist: "case/client/emp",
