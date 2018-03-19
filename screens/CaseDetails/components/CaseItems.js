92956d632a87315f5ab9fff413c597e9c5cf0840
DivineBrad <bradleymjblanchard@gmail.com>
2018-03-19 00:15:36 -0400

    Case Items loading items base on caseid from parent

--- screens/CaseDetails/components/CaseItems.js
+++ screens/CaseDetails/components/CaseItems.js
@@ -1,78 +1,61 @@
 import React, { Component } from 'react';
-import { AsyncStorage, ActivityIndicator, ListView, Text, View } from 'react-native';
+import { TouchableHighlight, AsyncStorage, ActivityIndicator, ListView, Text, View } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 import axios from 'axios';
-import config from "./../../../assets/config/endpoint";
+import endpoint from "./../../../assets/config/endpoint";
+import { StackNavigator } from 'react-navigation';
 import styles from "./../styles/CaseItemsStyles";
 import color from "./../../../assets/styles/color";
 
-export default class TestItems1 extends Component {
+export default class CaseItems extends Component {
     constructor(props) {
         super(props);
         this.state = {
             isLoading: true,
-            token: ''
-
+            token: '',
+            caseItems: [],
+            dataSource:[],
+            caseId:'',
+            caseItemId: ''
         }
     }
-
-    //passinf caseid of list item being clicked
-    GetItem = () => {
+    
+    //fired on press of case item, to open new window with item
+    getItem() {
         console.log("clicked");
-        // this.props.navigation.navigate(
-        //     'CaseItemDetails',
-        //     { caseitemid :caseitemid },
-        //   );
+         /*this.props.navigation.navigate(
+             'CaseItemDetails',
+            { caseItemId :id},
+           ); */
     }
-
-    async componentDidMount() {
-        this.state.token = await AsyncStorage.getItem("token");
-        //this.state.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJzb2FpYiIsImV4cCI6MTUyMjM0MjExOSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwLyIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAwMC8ifQ.9pOx82l-_RhlyeJU-xBKlCg4B6UlmcDjv6PdMVH9qL4";
-        var url = config.api.url + config.api.endpoints.caseItems.caseItemsForCase + "37148";
-        axios({
-            method: "get",
-            url: url,
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + this.state.token,
-            }
-        })
-            .then(response => {
-                console.log(response.data);
-                let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-                this.setState({
-                    isLoading: false,
-                    dataSource: ds.cloneWithRows(response.data),
-                });
-            }
-            )
-            .catch(error => {
-                if (error.response) {
-                    // the response was other than 2xx status
-                    if (error.response.status == 401) {
-                        console.debug("Invalid username and password entered");
-                        this.authError();
-                    } else {
-                        console.error("Invalid request sent. Status : " + error.response.status);
-                        this.appError();
-                    }
-                } else {
-                    console.error("Something went wrong in the request Status : " + error.response.status + " Response : " + error);
-                    this.appError();
-                }
-            });
-
+    async componentWillMount(){
+         // Get case items from parent 
+         console.log("Inside component did mount Case Items");
+         // Get State for caseId       
+          this.state.caseId=this.props.CaseId;
+          this.state.token = await AsyncStorage.getItem("token");
+          console.log("Case ID inside of Case Items is "+this.state.caseId)
+          this.getCaseItems();
     }
-    checkStatus() {
-
-    }
+    
+    checkStatus= ()=> {
+        console.log("Check Status Method");
+    };
 
     renderRow(rowData, sectionID) {
+        getItem=() =>{
+            console.log("clicked");
+             this.props.navigation.navigate(
+                 'CaseItemDetails',
+                { caseItemId :id},
+               ); 
+        }
         return (
+           
             <ListItem
-
-                key={sectionID}
+          //  onPress= {() => this.props.navigation.navigate('CaseItemDetails', {CaseItemId:'275314'})}
+          onPress= {() =>this.props.openCaseItems('275314')}
+                    key={sectionID}
                 //Content properties
                 title={<View ><Text style={styles.title} numberOfLines={1}>Status: <Text style={styles.status}>{rowData.caseItemStatus}</Text></Text>
 
@@ -80,12 +63,14 @@ export default class TestItems1 extends Component {
                 subtitle={<Text style={styles.itemDesc} numberOfLines={1}>{rowData.caseItemDescription}</Text>}
                 //Styling properties
                 rightTitle={rowData.caseItemDate}
-                onPress={this.GetItem}
+                
+               //onPress= {() => this.props.navigation.navigate('CaseItemDetails')}
                 rightTitleStyle={styles.itemDate}
                 chevronColor={color.primaryColor.hex}
                 containerStyle={styles.container}
 
             />
+            
         )
     }
 
@@ -98,18 +83,61 @@ export default class TestItems1 extends Component {
             );
         }
         return (
+           
             <View style={{ flex: 1 }}>
                 <List containerStyle={{ marginBottom: 20 }}>
 
                     <ListView
                         dataSource={this.state.dataSource}
                         renderRow={this.renderRow}
-
+                       
                         
                     />
 
                 </List>
-            </View>
+            </View> 
         );
     }
+
+
+    getCaseItems(){
+       
+        var url = endpoint.api.url + endpoint.api.endpoints.caseItems.caseItemsForCase +  this.state.caseId;
+        console.log("Calling "+url);
+        axios({
+            method: "get",
+            url: url,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + this.state.token,
+            }
+        })
+            .then(response => {
+                console.log(response.data);
+               
+                let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+                this.setState({
+                    isLoading: false,
+                    dataSource: ds.cloneWithRows(response.data),
+                    }); 
+                console.log("Case Items Data :" + response.data);
+            }
+            )
+            .catch(error => {
+                if (error.response) {
+                    // the response was other than 2xx status
+                    if (error.response.status == 401) {
+                        console.debug("Invalid username and password entered");
+                       // this.authError();
+                    } else {
+                        console.error("Invalid request sent. Status : " + error.response.status);
+                        //this.appError();
+                    }
+                } else {
+                    console.error("Something went wrong in the request Status : " +  error.response.status+ " Response : " + error);
+                    //this.appError();
+                }
+            });
+         }
 }
