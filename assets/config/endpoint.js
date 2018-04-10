28def0cc4459e15e4dbdbcc175a8ec6e2bf7c77b
Mohit Deshpande <mohitdeshpande@icloud.com>
2018-04-10 10:06:05 -0400

    update endpoint

--- assets/config/endpoint.js
+++ assets/config/endpoint.js
@@ -1,6 +1,6 @@
 export default {
   api: {
-    url: "http://localhost:5000/api/",
+    url: "https://momentuumapi.azurewebsites.net/api/",
     endpoints: {
       login: "auth/login",
       caselist: "case/client/emp",
